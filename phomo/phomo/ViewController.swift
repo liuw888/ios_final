e73f8c341c197359fe437b287b7ebae88c5c5ba3
Wenzhe Liu <liuw888@ekans.cs.uchicago.edu>
2020-03-05 11:18:09 -0600

    first trial

---
+++ phomo/phomo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  phomo
+//
+//  Created by Wenzhe Liu on 3/5/20.
+//  Copyright © 2020 Wenzhe Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
